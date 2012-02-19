4fabbe1831a56214066004cf4472cad5d8c1aa3d
miroslav.miroslavov@gmail.com <miroslav.miroslavov@gmail.com@9ad0c900-99ea-0ab4-ac42-22058a0f8feb>
2012-02-19 17:24:53 +0000

    FactoryMethod - Working on the examples.

--- trunk/resources/Chapter-08-Factory-Method/FactoryMethod/src/ModuleFactory.java
+++ trunk/resources/Chapter-08-Factory-Method/FactoryMethod/src/ModuleFactory.java
@@ -1,8 +1,27 @@
 public class ModuleFactory {
+	private Module defaultModule;
+
 	public final Module createModule() {
 		Module module = createModuleOverride();
 		// Register the module in global repository.
 		return module;
+	}
+
+	public final Module createModule(Modules module) {
+		switch (module) {
+		case ShapeModul:
+			return new ShapeModule();
+		case LinkModul:
+			// return new LinkModule();
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public final Module getDefaultModule() {
+		if (defaultModule == null) {
+			defaultModule = new ShapeModule();
+		}
+		return defaultModule;
 	}
 
 	protected Module createModuleOverride() {
---
+++ trunk/resources/Chapter-08-Factory-Method/FactoryMethod/src/Modules.java
@@ -0,0 +1,4 @@
+public enum Modules {
+	ShapeModul, LinkModul
+	//...
+}
