8a76c3685f24876d6d905e1bf5a3206613bf6b31
rossi.lazova <rossi.lazova@9ad0c900-99ea-0ab4-ac42-22058a0f8feb>
2012-03-04 23:28:22 +0000

    Update on the example code

--- trunk/resources/Chapter-27-Template-Method/TemplateMethodAdvanced/src/EatFruit.java
+++ trunk/resources/Chapter-27-Template-Method/TemplateMethodAdvanced/src/EatFruit.java
@@ -1,21 +1,10 @@
-/**
- * 
- */
-
-/**
- * @author I056723
- * 
- */
 public class EatFruit {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Apple apple = new Apple();
 		Banana banana = new Banana();
 
-		apple.eat();
-		banana.eat();
+		apple.consume();
+		banana.consume();
 	}
 }
