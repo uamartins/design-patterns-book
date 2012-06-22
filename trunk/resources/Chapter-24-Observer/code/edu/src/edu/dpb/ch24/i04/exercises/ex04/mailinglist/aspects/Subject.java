ed69a8eabfc67e2f47f6f4a83b947020e9d03bea
tsvetan.vasilev@gmail.com <tsvetan.vasilev@gmail.com@9ad0c900-99ea-0ab4-ac42-22058a0f8feb>
2012-06-22 04:05:43 +0000

    [ch24-Observer] Added 'mailingList' package in 'Exercises' section.

---
+++ trunk/resources/Chapter-24-Observer/code/edu/src/edu/dpb/ch24/i04/exercises/ex04/mailinglist/aspects/Subject.java
@@ -0,0 +1,57 @@
+package edu.dpb.ch24.i04.exercises.ex04.mailinglist.aspects;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class Subject {
+	protected Map<Role, Set<Observer>> role2observers;
+
+	public void register(Observer observer, Role... roles) {
+		if (roles == null) {
+			return;
+		}
+		for (Role role : roles) {
+			Set<Observer> observers = role2observers.get(role);
+			if (observers == null) {
+				observers = new HashSet<Observer>();
+				observers.add(observer);
+				role2observers.put(role, observers);
+			} else {
+				observers.add(observer);
+			}
+			System.out.println("[Subject] Registered " + observer
+					+ " to listen for messages from " + role + ".");
+		}
+
+	}
+
+	public void unregister(Observer observer, Role... roles) {
+		if (roles == null) {
+			return;
+		}
+		for (Role role : roles) {
+			Set<Observer> observers = role2observers.get(role);
+			if (observers != null) {
+				observers.remove(observer);
+			}
+			if (observers.isEmpty()) {
+				role2observers.remove(role);
+			}
+			System.out.println("[Subject] Unregistered " + observer
+					+ " to listen for messages from " + role + ".");
+		}
+	}
+
+	public void notifyObservers(Role role) {
+		System.out.println("[Subject] Notify all observers "
+				+ "for new message from " + role + ".");
+
+		Set<Observer> observers = role2observers.get(role);
+		if (observers != null) {
+			for (Observer observer : observers) {
+				observer.update();
+			}
+		}
+	}
+}
