25cc9fdb2656cf4504f2ebfec41cead056a2ef49
rossi.lazova@gmail.com <rossi.lazova@gmail.com@9ad0c900-99ea-0ab4-ac42-22058a0f8feb>
2012-07-02 00:28:28 +0000

    Adding a new Java Eclipse project with the Template Method applied for a binary search

---
+++ trunk/resources/Chapter-27-Template-Method/TemplateMethodBinarySearch/src/EatFruit.java
@@ -0,0 +1,11 @@
+public class EatFruit {
+
+	public static void main(String[] args) {
+		Apple apple = new Apple(5);
+		Banana banana = new Banana(2,7);
+
+		apple.consume();
+		banana.consume();
+	}
+
+}
