f2b50580d9f5179ab77d9c9cab61138008a9ec37
bsivanov <bsivanov@9ad0c900-99ea-0ab4-ac42-22058a0f8feb>
2013-01-27 22:23:45 +0000

    [Chapter-23-Memento] Added part of 'Known Applications section. Modified example's source code and VPP diagrams.

--- trunk/resources/Chapter-23-Memento/Chapter-23-Memento-Examples/src/OperatingSystem.java
+++ trunk/resources/Chapter-23-Memento/Chapter-23-Memento-Examples/src/OperatingSystem.java
@@ -1,25 +1,23 @@
-
 public class OperatingSystem {
-	private Object timerSettings;
 	private TimerApplication timerApplication;
-	
+	private TimerApplication.Settings timerSettings;
+
 	public void startTimerApplication() {
 		this.timerApplication = new TimerApplication();
-        
+
 		if (this.timerSettings != null) {
 			this.timerApplication.setSettings(this.timerSettings);
 		}
-		
+
 		this.timerApplication.start();
 	}
-	
+
 	public void stopTimerApplication() {
 		if (this.timerApplication != null) {
 			this.timerApplication.stop();
 			this.timerSettings = this.timerApplication.getSettings();
-			
+
 			System.out.println(this.timerApplication.getMilliseconds());
 		}
 	}
-
 }
--- trunk/resources/Chapter-23-Memento/Chapter-23-Memento-Examples/src/Program.java
+++ trunk/resources/Chapter-23-Memento/Chapter-23-Memento-Examples/src/Program.java
@@ -1,25 +1,22 @@
-
 public class Program {
-
 	/**
 	 * @param args
-	 * @throws InterruptedException 
+	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		OperatingSystem operatingSystem = new OperatingSystem();
-		
+
 		operatingSystem.startTimerApplication();
-		Thread.sleep(2000);		
+		Thread.sleep(2000);
 		operatingSystem.stopTimerApplication();
-		
+
 		operatingSystem.startTimerApplication();
-		Thread.sleep(2000);		
+		Thread.sleep(2000);
 		operatingSystem.stopTimerApplication();
-		
+
 		operatingSystem.startTimerApplication();
-		Thread.sleep(2000);		
+		Thread.sleep(2000);
 		operatingSystem.stopTimerApplication();
 	}
-
 }
