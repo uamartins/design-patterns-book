98d2f570af2659c4c3d6a5ac9672df3039b3dba0
radoslav.h.i@gmail.com <radoslav.h.i@gmail.com@9ad0c900-99ea-0ab4-ac42-22058a0f8feb>
2013-02-19 20:43:00 +0000

    Facade - updated diagrams and source files

---
+++ resources/Chapter-15-Facade/Facade-Demo/Facade/src/Heater.java
@@ -0,0 +1,22 @@
+public class Heater {
+
+	public void turnOn() {
+		System.out.println("Включване на нагревател");
+	}
+
+	public void turnOff() {
+		System.out.println("Изключване на нагревател");
+	}
+
+	public void bakeBread() {
+		System.out.println("Печене на хляб");
+	}
+	
+	public void boilJam() {
+		System.out.println("Варене на конфитюр");
+	}
+	
+	public void boilCompote() {
+		System.out.println("Варене на компот");
+	}
+}
