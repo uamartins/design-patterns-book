7b5a55a644fd232f77f8330fc42591f0eae96e6a
radoslav.h.i@gmail.com <radoslav.h.i@gmail.com@9ad0c900-99ea-0ab4-ac42-22058a0f8feb>
2013-02-19 20:43:00 +0000

    Facade - updated diagrams and source files

---
+++ trunk/resources/Chapter-15-Facade/Facade-Demo/Facade/src/Cutter.java
@@ -0,0 +1,18 @@
+public class Cutter {
+
+	public void turnOn() {
+		System.out.println("Включване на машина за рязане");
+	}
+
+	public void turnOff() {
+		System.out.println("Изключване на машина за рязане");
+	}
+
+	public void cutDough() {
+		System.out.println("Рязане на тесто");
+	}
+	
+	public void cutFruits() {
+		System.out.println("Рязане на плодове");
+	}
+}
